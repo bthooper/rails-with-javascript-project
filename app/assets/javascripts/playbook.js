30d9ebc7faa281ee6b85dc56634ee59078bfb527
Bryan Hooper <bryan@lindsayfarm.net>
2019-07-08 10:25:03 -0400

    connecting up javascript, initial efforts

---
+++ app/assets/javascripts/playbook.js
@@ -0,0 +1,7 @@
+const playAnchorElement = document.getElementById('Ground Game');
+console.log(playAnchorElement);
+
+// playAnchorElement.onClick(function (e) {
+//   e.preventDefault;
+//   alert('ZZ Top!!');
+// });
