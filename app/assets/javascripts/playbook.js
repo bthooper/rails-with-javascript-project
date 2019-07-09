574832c36924832183b71318f6d91c8a8a3084da
Bryan Hooper <bthooper@gmail.com>
2019-07-09 11:00:29 -0400

    started working on primary fetch function for playbooks

--- app/assets/javascripts/playbook.js
+++ app/assets/javascripts/playbook.js
@@ -4,23 +4,53 @@ $(document).on("turbolinks:load", function() {
   $('.playbook_anchor').on('click', function(e) {
     e.preventDefault();
     console.log(e);
-    alert('You clicked ' + e.target);
+    console.log(e.target.baseURI);
 
     // We need to make a request for the playbook data we want, and then 
     // append it to the li item so that it is displayed on the page
     // without a reload.  AJAX!
     
-    getPlaybookDataFromUrl(e.target);
-    appendData();
+    const playbook_url = e.target.baseURI;
+
+    console.log("PB URL: " + playbook_url);
+    
+    const data = getPlaybookDataFromUrl(playbook_url);
+
+    // console.log(data);
+
+    // You will get JS object back in data above.  So you 
+    // may need to deal with that object in some way
+    // before you append it.
+    
+    // appendData(data);
     
     
   });
 
-  function getPlaybookDataFromUrl() {
+  function getPlaybookDataFromUrl(url) {
+
+    //  Use fetch to get data from RoR api.
+    //  Format that data and capture it in a JS object
+    //  return that JS object 
+    //
+    fetch(url)
+    .then(function(resp) {
+      return resp.text();
+    })
+    .then(function(resp) {
+      console.log(resp);
+    });
+    
+
+    // return data;
+    //
+    //
+    return "awesome";
+    
 
   }
 
-  function appendData() {
+  function appendData(data) {
 
   }
 
