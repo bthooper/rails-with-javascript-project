01b0f411da88c628619a2572c470372937ee1014
Bryan Hooper <bthooper@gmail.com>
2019-07-08 21:07:24 -0400

    javascript working with turbolinks workaround

--- app/assets/javascripts/playbook.js
+++ app/assets/javascripts/playbook.js
@@ -1,7 +1,24 @@
-const playAnchorElement = document.getElementById('Ground Game');
-console.log(playAnchorElement);
+// Turbolinks is installed, so we need to start with:
+$(document).on("turbolinks:load", function() {
 
-// playAnchorElement.onClick(function (e) {
-//   e.preventDefault;
-//   alert('ZZ Top!!');
-// });
+  $("#Camel").click(function(e) {
+    e.preventDefault;
+    console.log("WOWZERS!");
+  });
+
+  $('.playbook_anchor').on('click', function(e) {
+    e.preventDefault();
+    console.log(e);
+    alert('You clicked ' + e.target);
+
+    
+
+    // We need to make a request for the playbook data we want, and then 
+    // append it to the li item so that it is displayed on the page
+    // without a reload.  AJAX!
+    
+  });
+
+
+
+});
