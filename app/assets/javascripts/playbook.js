10a869757bc45960046aa013234d53de20a12f7e
Bryan Hooper <bthooper@gmail.com>
2019-07-08 21:15:50 -0400

    cleaned up code and added notes and stub functions

--- app/assets/javascripts/playbook.js
+++ app/assets/javascripts/playbook.js
@@ -1,23 +1,28 @@
 // Turbolinks is installed, so we need to start with:
 $(document).on("turbolinks:load", function() {
 
-  $("#Camel").click(function(e) {
-    e.preventDefault;
-    console.log("WOWZERS!");
-  });
-
   $('.playbook_anchor').on('click', function(e) {
     e.preventDefault();
     console.log(e);
     alert('You clicked ' + e.target);
 
-    
-
     // We need to make a request for the playbook data we want, and then 
     // append it to the li item so that it is displayed on the page
     // without a reload.  AJAX!
     
+    getPlaybookDataFromUrl(e.target);
+    appendData();
+    
+    
   });
+
+  function getPlaybookDataFromUrl() {
+
+  }
+
+  function appendData() {
+
+  }
 
 
 
